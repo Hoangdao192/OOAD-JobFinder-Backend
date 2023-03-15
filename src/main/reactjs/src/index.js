24d42a060927040adf4d71719e608931ef78f2bc
Huyen <thanhhuyen11102k2@gmail.com>
2023-03-15 13:23:50 +0700

    feat: add sign in page

--- src/main/reactjs/src/index.js
+++ src/main/reactjs/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <GoogleOAuthProvider clientId="86747382708-83plp3crshp15iefdlpgpgjpc22tfhqe.apps.googleusercontent.com">
     <React.StrictMode>
-            <App />
+      <App />
     </React.StrictMode>
+  </GoogleOAuthProvider>
 );
-
--- src/main/reactjs/src/routes/Routes.js
+++ src/main/reactjs/src/routes/Routes.js
@@ -1,17 +1,18 @@
-import { Home } from "../pages/Home/Home"
-import BlankLayout from "../components/layouts/BlankLayout/BlankLayout"
-
+import { Home } from "../pages/Home";
+import BlankLayout from "../components/layouts/BlankLayout/BlankLayout";
+import SignIn from "../pages/auth/SignIn";
 const privateRoutes = [
-    //  Example: {path: '/manager/home', layout: ManagerLayout , component: ManagerHome, authorization : ['Admin']}
-    //  Explain:
-    //  path: path from root
-    //  layout: layout for this page (BlankLayout as default)
-    //  component: component will be display
-    //  authorization: array of roles can access this page
-]
+  //  Example: {path: '/manager/home', layout: ManagerLayout , component: ManagerHome, authorization : ['Admin']}
+  //  Explain:
+  //  path: path from root
+  //  layout: layout for this page (BlankLayout as default)
+  //  component: component will be display
+  //  authorization: array of roles can access this page
+];
 const publicRoutes = [
-    //  Example: {path: '/login', component: Login}
-    {path: '/', component: Home, layout: BlankLayout}
-]
+  //  Example: {path: '/login', component: Login}
+  { path: "/", component: Home, layout: BlankLayout },
+  { path: "/auth/signin", component: SignIn, layout: BlankLayout },
+];
 
-export { publicRoutes, privateRoutes }
+export { publicRoutes, privateRoutes };
